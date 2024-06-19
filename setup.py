df792c1410f748f0666a493f7706576e7117792a
FlacSy <flacsy.x@gmail.com>
2024-06-19 14:16:20 +0200

    1.0.0

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as fh:
+with open("requirements.txt", "r", encoding="utf-8") as fh:
     install_requires = fh.readlines()
 
 setup(
     name="dsplayer",
-    version="0.1.0",
+    version="1.0.0",
     description="Music player for Discord",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="FlacSy",
     author_email="flacsy.x@gmail.com",
     url="https://github.com/FlacSy/dsplayer",
-    install_requires = install_requires,
-    python_requires=">=3.6",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    python_requires=">=3.10.0",
 )
